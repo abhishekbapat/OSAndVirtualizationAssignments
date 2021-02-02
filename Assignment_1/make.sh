c9d4c8785e13d648854e5cd9555811d283709704
abhishek <abhishek@ubuntu-laptop.local>
2021-02-02 11:11:12 -0500

    chore: setup done~

--- Assignment_1/make.sh
+++ Assignment_1/make.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Clear old stuff
+rm boot.iso boot.o kernel kernel.o boot.dll boot.efi boot.lib
+
 # Compile the boot loader
 clang -m64 -O2 -fshort-wchar -I ../Include -I ../Include/X64 -mcmodel=small -mno-red-zone -mno-stack-arg-probe -target x86_64-pc-mingw32 -Wall -c boot.c
 lld-link /dll /nodefaultlib /safeseh:no /machine:AMD64 /entry:efi_main boot.o /out:boot.dll
