f818e8e25b79e03bed2fb6c1720c5cc150adb97c
Abhishek Bapat <abhishekbapat@Abhisheks-MacBook-Pro.local>
2021-02-02 09:48:28 -0500

    first commit

---
+++ Assignment_1/Code/boot.c
@@ -0,0 +1,157 @@
+/*
+ * boot.c - a boot loader template (Assignment 1, ECE 6504)
+ * Copyright 2021 Ruslan Nikolaev <rnikola@vt.edu>
+ */
+
+#include <Uefi.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Protocol/GraphicsOutput.h>
+
+/* Use GUID names 'gEfi...' that are already declared in Protocol headers. */
+EFI_GUID gEfiLoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+EFI_GUID gEfiSimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+EFI_GUID gEfiGraphicsOutputProtocolGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+
+/* Keep these variables global. */
+static EFI_HANDLE ImageHandle;
+static EFI_SYSTEM_TABLE *SystemTable;
+static EFI_BOOT_SERVICES *BootServices;
+
+// Currently uses only EfiBootServicesData
+// The function can be extended to accept any type as necessary
+static VOID *AllocatePool(UINTN size)
+{
+	VOID *ptr;
+	EFI_STATUS ret = BootServices->AllocatePool(EfiBootServicesData, size, &ptr);
+	if (EFI_ERROR(ret))
+		return NULL;
+	return ptr;
+}
+
+static VOID FreePool(VOID *buf)
+{
+	BootServices->FreePool(buf);
+}
+
+static EFI_STATUS OpenKernel(EFI_FILE_PROTOCOL **pvh, EFI_FILE_PROTOCOL **pfh)
+{
+	EFI_LOADED_IMAGE *li = NULL;
+	EFI_FILE_IO_INTERFACE *fio = NULL;
+	EFI_FILE_PROTOCOL *vh;
+ 	EFI_STATUS efi_status;
+
+	*pvh = NULL;
+	*pfh = NULL;
+
+	efi_status = BootServices->HandleProtocol(ImageHandle,
+					&gEfiLoadedImageProtocolGuid, (void **) &li);
+	if (EFI_ERROR(efi_status)) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+			L"Cannot get LoadedImage for BOOTx64.EFI\r\n");
+		return efi_status;
+	}
+
+	efi_status = BootServices->HandleProtocol(li->DeviceHandle,
+					&gEfiSimpleFileSystemProtocolGuid, (void **) &fio);
+	if (EFI_ERROR(efi_status)) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+						L"Cannot get fio\r\n");
+		return efi_status;
+	}
+
+	efi_status = fio->OpenVolume(fio, pvh);
+	if (EFI_ERROR(efi_status)) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+						L"Cannot get the volume handle!\r\n");
+		return efi_status;
+	}
+	vh = *pvh;
+
+	efi_status = vh->Open(vh, pfh, L"\\EFI\\BOOT\\KERNEL",
+					EFI_FILE_MODE_READ, 0);
+	if (EFI_ERROR(efi_status)) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+						L"Cannot get the file handle!\r\n");
+		return efi_status;
+	}
+
+	return EFI_SUCCESS;
+}
+
+static void CloseKernel(EFI_FILE_PROTOCOL *vh, EFI_FILE_PROTOCOL *fh)
+{
+	vh->Close(vh);
+	fh->Close(fh);
+}
+
+static UINT32 *SetGraphicsMode(UINT32 width, UINT32 height)
+{
+	EFI_GRAPHICS_OUTPUT_PROTOCOL *graphics;
+	EFI_STATUS efi_status;
+	UINT32 mode;
+
+	efi_status = BootServices->LocateProtocol(&gEfiGraphicsOutputProtocolGuid,
+                    NULL, (VOID **) &graphics);
+	if (EFI_ERROR(efi_status)) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+			L"Cannot get the GOP handle!\r\n");
+		return NULL;
+	}
+
+	if (!graphics->Mode || graphics->Mode->MaxMode == 0) {
+		SystemTable->ConOut->OutputString(SystemTable->ConOut,
+			L"Incorrect GOP mode information!\r\n");
+		return NULL;
+	}
+
+	for (mode = 0; mode < graphics->Mode->MaxMode; mode++) {
+		EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *info;
+		UINTN size;
+
+
+		// TODO: Locate BlueGreenRedReserved, aka BGRA (8-bit per color)
+		// Resolution width x height (800x600 in our code)
+
+		// Activate (set) this graphics mode
+
+		// Return the frame buffer base address
+		return (UINT32 *) graphics->Mode->FrameBufferBase;
+	}
+	return NULL;
+}
+
+/* Use System V ABI rather than EFI/Microsoft ABI. */
+typedef void (*kernel_entry_t) (unsigned int *, int, int) __attribute__((sysv_abi));
+
+EFI_STATUS EFIAPI
+efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
+{
+	EFI_FILE_PROTOCOL *vh, *fh;
+	EFI_STATUS efi_status;
+	UINT32 *fb;
+
+	ImageHandle = imageHandle;
+	SystemTable = systemTable;
+	BootServices = systemTable->BootServices;
+
+	efi_status = OpenKernel(&vh, &fh);
+	if (EFI_ERROR(efi_status)) {
+		BootServices->Stall(5 * 1000000); // 5 seconds
+		return efi_status;
+	}
+
+	// TODO: Load the kernel
+
+	CloseKernel(vh, fh);
+
+	fb = SetGraphicsMode(800, 600);
+
+	// kernel's _start() is at base #0 (pure binary format)
+	// cast the function pointer appropriately and call the function
+	//
+	// kernel_entry_t func = (kernel_entry_t) buffer;
+	// func(fb, 800, 600);
+
+	return EFI_SUCCESS;
+}
---
+++ Assignment_1/Code/kernel.c
@@ -0,0 +1,13 @@
+/*
+ * boot.c - a kernel template (Assignment 1, ECE 6504)
+ * Copyright 2021 Ruslan Nikolaev <rnikola@vt.edu>
+ */
+
+void _start(unsigned int *framebuffer, int width, int height)
+{
+	/* TODO: Draw some simple figure (rectangle, square, etc)
+	   to indicate kernel activity. */
+
+	/* Never exit! */
+	while (1) {};
+}
