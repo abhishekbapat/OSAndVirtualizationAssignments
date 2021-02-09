9bef525b60a0d24374b7c27c3df416c384b09c1c
abhishek <abhishek@ubuntu-laptop.local>
2021-02-09 15:43:33 -0500

    chore: assignment 1 q1 and q2 completed

--- Assignment_1/boot.c
+++ Assignment_1/boot.c
@@ -22,10 +22,10 @@ static EFI_BOOT_SERVICES *BootServices;
 
 // Currently uses only EfiBootServicesData
 // The function can be extended to accept any type as necessary
-static VOID *AllocatePool(UINTN size)
+static VOID *AllocatePool(UINTN size, EFI_MEMORY_TYPE memoryType)
 {
 	VOID *ptr;
-	EFI_STATUS ret = BootServices->AllocatePool(EfiBootServicesData, size, &ptr);
+	EFI_STATUS ret = BootServices->AllocatePool(memoryType, size, &ptr);
 	if (EFI_ERROR(ret))
 		return NULL;
 	return ptr;
@@ -101,7 +101,7 @@ static EFI_STATUS ReadFileSize(EFI_FILE_PROTOCOL *fh, UINTN *file_size)
 	efi_status = fh->GetInfo(fh, &gEfiFileInfoGuid, &file_info_size, NULL);
 	if (efi_status == EFI_BUFFER_TOO_SMALL)
 	{
-		file_info = AllocatePool(file_info_size);
+		file_info = AllocatePool(file_info_size, EfiBootServicesData);
 	}
 	else
 	{
@@ -200,20 +200,20 @@ static UINT32 *SetGraphicsMode(UINT32 width, UINT32 height)
 	return frameBufferDefault;
 }
 
-static EFI_STATUS GetMemoryMapKey(UINTN *mapKey)
+static EFI_STATUS ExitBootServicesHook(EFI_HANDLE imageHandle)
 {
 	UINTN descriptorSize;
 	UINT32 descriptorVersion;
 	EFI_MEMORY_DESCRIPTOR *memoryMap;
 	EFI_STATUS efi_status;
 	UINTN memoryMapSize = 0;
-	UINTN tempMemoryMapKey;
+	UINTN memoryMapKey;
 	memoryMap = NULL;
 
-	efi_status = BootServices->GetMemoryMap(&memoryMapSize, memoryMap, &tempMemoryMapKey, &descriptorSize, &descriptorVersion);
+	efi_status = BootServices->GetMemoryMap(&memoryMapSize, memoryMap, &memoryMapKey, &descriptorSize, &descriptorVersion);
 	if (efi_status == EFI_BUFFER_TOO_SMALL)
 	{
-		memoryMap = AllocatePool(memoryMapSize);
+		memoryMap = AllocatePool(memoryMapSize, EfiBootServicesData);
 	}
 	else
 	{
@@ -222,26 +222,19 @@ static EFI_STATUS GetMemoryMapKey(UINTN *mapKey)
 		return efi_status;
 	}
 
-	efi_status = BootServices->GetMemoryMap(&memoryMapSize, memoryMap, &tempMemoryMapKey, &descriptorSize, &descriptorVersion);
+	efi_status = BootServices->GetMemoryMap(&memoryMapSize, memoryMap, &memoryMapKey, &descriptorSize, &descriptorVersion);
 	if (EFI_ERROR(efi_status))
 	{
 		SystemTable->ConOut->OutputString(SystemTable->ConOut,
 										  L"Error reading the memory map key.\r\n");
-	}
-	else
-	{
-		*mapKey = tempMemoryMapKey;
+		return efi_status;
 	}
 
-	FreePool(memoryMap);
+	//FreePool(memoryMap);
+
+	BootServices->ExitBootServices(imageHandle, memoryMapKey);
 
 	return efi_status;
-}
-
-static void PrintMessage(UINTN delay_seconds, CHAR16 *message)
-{
-	SystemTable->ConOut->OutputString(SystemTable->ConOut, message);
-	BootServices->Stall(delay_seconds * 1000000);
 }
 
 /* Use System V ABI rather than EFI/Microsoft ABI. */
@@ -266,7 +259,6 @@ efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
 		BootServices->Stall(5 * 1000000); // 5 seconds
 		return efi_status;
 	}
-	PrintMessage(2, L"Opened kernel file for reading. \r\n");
 
 	efi_status = ReadFileSize(fh, &file_size);
 	if (EFI_ERROR(efi_status))
@@ -274,49 +266,33 @@ efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
 		BootServices->Stall(5 * 1000000); // 5 seconds
 		return efi_status;
 	}
-	PrintMessage(2, L"Read kernel file size. \r\n");
 
-	// buffer = AllocatePool(file_size);
-	// efi_status = LoadKernel(fh, file_size, buffer);
-	// if (EFI_ERROR(efi_status))
-	// {
-	// 	BootServices->Stall(5 * 1000000); // 5 seconds
-	// 	return efi_status;
-	// }
-	// PrintMessage(2, L"Loaded kernel. \r\n");
+	buffer = AllocatePool(file_size, EfiPersistentMemory);
+	efi_status = LoadKernel(fh, file_size, buffer);
+	if (EFI_ERROR(efi_status))
+	{
+		BootServices->Stall(5 * 1000000); // 5 seconds
+		return efi_status;
+	}
 
 	CloseKernel(vh, fh);
-	PrintMessage(2, L"Closed kernel file. \r\n");
 
-	// fb = SetGraphicsMode(800, 600);
-	// PrintMessage(2, L"Graphics mode set to 800x600. \r\n");
+	fb = SetGraphicsMode(800, 600);
+
+	efi_status = ExitBootServicesHook(ImageHandle);
+	if (EFI_ERROR(efi_status))
+	{
+		BootServices->Stall(5 * 1000000); // 5 seconds
+		return efi_status;
+	}
 
 	// kernel's _start() is at base #0 (pure binary format)
 	// cast the function pointer appropriately and call the function
-	//
-	// kernel_entry_t func = (kernel_entry_t)buffer;
-	// func(fb, 800, 600);
+
+	kernel_entry_t func = (kernel_entry_t)buffer;
+	func(fb, 800, 600);
 
 	// FreePool(buffer);
-
-	UINTN mapKey = 0;
-	efi_status = GetMemoryMapKey(&mapKey);
-	if (EFI_ERROR(efi_status))
-	{
-		BootServices->Stall(5 * 1000000);
-		return efi_status;
-	}
-	PrintMessage(2, L"Got memory map key. \r\n");
-
-	PrintMessage(2, L"Exiting boot services. \r\n");
-	efi_status = BootServices->ExitBootServices(imageHandle, mapKey);
-	if (EFI_ERROR(efi_status))
-	{
-		SystemTable->ConOut->OutputString(SystemTable->ConOut,
-										  L"Error exiting boot services!\r\n");
-		BootServices->Stall(5 * 1000000);
-		return efi_status;
-	}
 
 	return EFI_SUCCESS;
 }
--- Assignment_1/kernel.c
+++ Assignment_1/kernel.c
@@ -4,35 +4,35 @@
  */
 
 //TODO: Figure out how to correctly draw.
+void draw_rect(int, int, int, int, int, unsigned int *);
+
 void kernel_start(unsigned int *framebuffer, int width, int height)
 {
 	/* Draw some simple figure (rectangle, square, etc)
 	   to indicate kernel activity. */
 
 	int colour = 0x2596beff;
-	int centerHeight = (height/2)-1;
-	int centerWidth = (width/2)-1;
-	int rectHeight = 500;
-	int rectWidth = 700;
+	int rectHeight = 200;
+	int rectWidth = 400;
 
-	for (int i = centerWidth-(rectWidth/2); i < centerWidth+(rectWidth/2); i++)
-	{
-		for (int j = centerHeight-(rectHeight/2); j < centerHeight+(rectHeight/2); j++)
-		{
-			framebuffer[i + width * j] = colour;
-		}
-	}
-
-	// for (int i = 0; i < rectWidth; i++)
-	// {
-	// 	for (int j = 0; j < rectHeight; j++)
-	// 	{
-	// 		framebuffer[i + width * j] = colour;
-	// 	}
-	// }
+	draw_rect(colour, rectWidth, rectHeight, width, height, framebuffer);
 
 	/* Never exit! */
 	while (1)
 	{
 	};
+}
+
+void draw_rect(int colour, int rectWidth, int rectHeight, int width, int height, unsigned int *framebuffer)
+{
+	int centerHeight = (height / 2) - 1;
+	int centerWidth = (width / 2) - 1;
+
+	for (int i = centerWidth - (rectWidth / 2); i < centerWidth + (rectWidth / 2); i++)
+	{
+		for (int j = centerHeight - (rectHeight / 2); j < centerHeight + (rectHeight / 2); j++)
+		{
+			framebuffer[i + width * j] = colour;
+		}
+	}
 }
